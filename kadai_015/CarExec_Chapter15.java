25b5e022fc8966874249855c033406115fe25c9f
hacccico <hacccico@gmail.com>
2024-08-04 16:34:23 +0900

    車のクラスの課題追加

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,16 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+	public static void main(String[] args) {
+		//車のインスタンスを作成
+		Car_Chapter15 car = new Car_Chapter15();
+		
+		//gearChangeメソッドを実行
+		car.gearChange(3);
+		
+		//runメソッドを実行
+		car.run();
+
+	}
+
+}
